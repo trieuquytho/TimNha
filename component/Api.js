bba1c6ee57b22da24ff20ae91825c1b691b422ac
trieuquytho <trieuquytho1997@gmail.com>
2019-10-14 00:07:13 +0700

    up code

---
+++ component/Api.js
@@ -0,0 +1,11 @@
+const UrlBase = "http://192.168.0.125:81/" + "BanHang/";
+
+const DangNhapService = UrlBase + "DangNhap.php";
+const GetToanBoTheLoaiService = UrlBase + "GetToanBoTheLoai.php";
+const GetHangTheoIdTheLoaiService = UrlBase + "GetHangTheoIdTheLoai.php";
+
+export {
+    DangNhapService,
+    GetToanBoTheLoaiService,
+    GetHangTheoIdTheLoaiService
+}
